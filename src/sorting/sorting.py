fdf5effd0187a4d16ecfbd391a8a4b39916e90b1
Jessica <duellje@oregonstate.edu>
2020-09-15 15:11:08 -0400

    completed merge sort

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -70,33 +70,64 @@ Ex.
 
 # TO-DO: complete the helper function below to merge 2 sorted arrays
 def merge(arrA, arrB):
-    elements = len(arrA) + len(arrB)
+    aLen = len(arrA)
+    bLen = len(arrB)
+    
+    elements = aLen + bLen
     merged_arr = [0] * elements
-
+    
     aIndex = 0
     bIndex = 0
     
-    while aIndex < len(arrA) or bIndex < len(arrB):
-        if arrA[aIndex] < arrB[bIndex]:
-            merged_arr.append(arrA[aIndex])
-            aIndex += 1
-        elif arrA[aIndex] > arrB[bIndex]:
-            merged_arr.append(arrB(bIndex))
+    for i in range(len(merged_arr)):
+        #check if at the end of the a arr
+        if aIndex > aLen - 1:
+            #since no more elements in a arr add rest of b arr elements
+            merged_arr[i] = arrB[bIndex]
             bIndex += 1
-        else:
-            merged_arr.append(arrA[aIndex])
-            merged_arr.append(arrB[bIndex])
+        #check if at end of b arr
+        elif bIndex > bLen - 1:
+            #since no more lements in b arr add rest of a arr elements
+            merged_arr[i] = arrA[aIndex]
             aIndex += 1
-            bIndex ++ 1
+        #not at the end of either array
+        else:
+            if arrA[aIndex] > arrB[bIndex]:
+                merged_arr[i] = arrB[bIndex]
+                bIndex += 1
+            else:
+                merged_arr[i] = arrA[aIndex]
+                aIndex += 1
     
     return merged_arr
 
 # TO-DO: implement the Merge Sort function below recursively
 def merge_sort(arr):
-    # Your code here
-
-
+    #find middle value
+    middle = len(arr) // 2
+    
+    #the left array holds all elements to the left of the middle value
+    left = arr[:middle]
+    
+    #right array holds all elements to the right of that
+    right = arr[middle:]    
+    
+    #if there's more than 1 number in the left array
+    if len(left) > 1:
+        left = merge_sort(left)
+    
+    #if there's more than 1 number in the right array
+    if len(right) > 1:
+        right = merge_sort(right)
+    
+    arr = merge(left, right)
+    
     return arr
+     
+    
+        
+        
+      
 
 # STRETCH: implement the recursive logic for merge sort in a way that doesn't 
 # utilize any extra memory
